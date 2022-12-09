466abebe4496d41dff2ad0bf630aa17889c83419
agonglomarel7 <agonglomarel@gmail.com>
2022-12-09 07:40:26 -0800

    Negative Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,15 @@
     
 int main (void){
 
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positif \n", n);
-}else if(n == 0){
-printf("%d is zero \n", n);
-}else {
-printf("%d is negatif\n", n);
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n > 0){
+        printf("%d is positif \n", n);
+    }else if (n == 0){
+        printf("%d is zero \n", n);
+    }else {
+        printf("%d is negatif\n", n);
+    }
+    return (0);
 }
-return (0);
-} 
-
