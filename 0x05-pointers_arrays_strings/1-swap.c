1b49056e8e2ca593725854d25c73e161e103a109
agonglomarel7 <agonglomarel@gmail.com>
2022-12-19 16:26:31 +0100

    Swap the value of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,8 +4,8 @@ Write a function that swaps the
 values of two integers.
 */
 void swap_int(int *a, int *b){
-    int temporary;
-    temporary = *b;
-    *b = *a;
-    *a = temporary;
+int temporary;
+temporary = *b;
+*b = *a;
+*a = temporary;
 }
