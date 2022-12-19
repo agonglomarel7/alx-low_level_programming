16f7cbb16850cadb08b28bf52fdab6629ed385e0
agonglomarel7 <agonglomarel@gmail.com>
2022-12-19 16:56:05 +0100

    Length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 /**
 * _strlen - gets character string
 *
 * @s: character to getstring
 * Return: returns length
 */
-int _strlen (char *s)
+int _strlen(char *s)
 {
-int i=0;
+int i = 0;
 int j;
 for (j = 0; s[j] != '\0'; j++)
 i++;
